bac86f2d703d2f4170f90c394e32e443a9ed27f5
Ruslan Potokin <ruslan.potokin@gmail.com>
2016-04-19 22:29:35 -0400

    Added a home view.

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-var myApp = angular.module('myApp', []);
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function($routeProvider) {
+    $routeProvider
+
+        .when('/', {
+            templateUrl : 'views/home.html',
+            controller  : 'mainController'
+        });
+});
 
 myApp.controller('mainController', function($scope) {
     $scope.message = 'Everyone come and see how good I look!';
